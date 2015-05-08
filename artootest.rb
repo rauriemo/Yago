52158cabfb0dbfd4dedfcd6e2fc3e85ba783e759
rauriemo <rafael.auriemo@gmail.com>
2015-05-08 11:31:16 -0700

    full keyboard movement implemented

--- artootest.rb
+++ artootest.rb
@@ -1,5 +1,8 @@
 require 'artoo'
 
+# connection :capture, :adaptor => :opencv_capture, :source=> "tcp://192.168.1.1:5555"
+# device :capture, :driver => :opencv_capture, :connection => :capture, :interval => 0.0
+
 connection :ardrone, :adaptor => :ardrone, :port => '192.168.1.1:5556'
 device :drone, :driver => :ardrone, :connection => :ardrone
 
@@ -10,28 +13,68 @@ connection :keyboard, adaptor: :keyboard
 device :keyboard, driver: :keyboard, connection: :keyboard
 
 
+
 work do
-  on drone, :ready => :fly
+  # on drone, :ready => :fly
   on keyboard, :key => :keypress
   drone.start(nav) # pass the nav object into the start method
+  p nav
+  p drone
 end
 
 def keypress(sender, key)
   p "*" * 80
   if key == "up"
-    drone.up
-  elsif key == "q"
-    drone.hover.land
-    drone.stop
+    p "im going up"
+    drone.up(0.1)
   elsif key == "down"
-    drone.down
+    p "im going down"
+    drone.down(0.1)
+  elsif key == "right"
+    p "im turning right"
+    drone.turn_right(0.1)
+  elsif key == "left"
+    p "im turning left"
+    drone.turn_left(0.1)
+  elsif key == "t"
+    p "im taking off"
+    drone.take_off
+  elsif key == "q"
+    p "im landing"
+    after(2.seconds) {drone.hover.land}
+    after(6.seconds) {drone.stop}
+  elsif key == "w"
+    p "im going forwards"
+    drone.forward(0.1)
+  elsif key == "s"
+    p "im going backwards"
+    drone.backward(0.1)
+  elsif key == "d"
+    p "im going right"
+    drone.right(0.1)
+  elsif key == "a"
+    p "im going left"
+    drone.left(0.1)
+  elsif key == "h"
+    p "im hovering"
+    drone.hover
   end
   p key
-  p sender
 end
 
 def fly(*data)
   drone.take_off
-  # after(15.seconds) { drone.hover.land }
-  # after(20.seconds) { drone.stop }
+  after(5.seconds) { drone.up(0.5) }
+  # after(5.seconds) { drone.animate(:flip_ahead, 1000) }
+  after(7.seconds) { drone.hover }
+  after(10.seconds) {drone.right(0.2)}
+  after(12.seconds) {drone.hover}
+  after(13.seconds) {drone.turn_right(0.01)}
+  after(17.seconds) { drone.hover.land }
+  after(22.seconds) { drone.stop }
+  # after(13.seconds) {
+  #   every(0.5){
+
+  #   }
+  #  }
 end
